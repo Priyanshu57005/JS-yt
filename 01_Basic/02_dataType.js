1ace64017cb15ded0ece6208a757a5f9242f966a
Priyanshu Gautam <priyanshu57005@gmail.com>
2025-03-19 19:47:59 +0000

    container config done

---
+++ 01_Basic/02_dataType.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+//alert( 3 + 3) // works with html/browswer and this is nodejs
+
+console.log(3 
+    + 3) // not a good practice this type of code
+
+console.log("Priyanshu")
+
+let name = "Priyanshu"
+let age = 18
+let isLoggedIn = true 
+let state = null
+
+
+
+// number => 2 to power 53
+// string => ""
+
+// boolean => true, false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+// object
+
+//console.log(typeof "Priyanshu") //string
+console.log(typeof undefined)
