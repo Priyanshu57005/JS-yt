f503fa4795227fbba3e5d2dde4794e7762c0df93
Priyanshu Gautam <priyanshu57005@gmail.com>
2025-03-19 19:21:03 +0000

    container config done

---
+++ 01_Basic/01_Variables.js
@@ -0,0 +1,24 @@
+const accountId = 144553
+let accountEmail = "priyanshu@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 //
+
+{
+    // curly braces are called scope in javascript
+}
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+accountEmail = "hfcca@gmail.com"
+accountPassword = "13216675465"
+accountCity = "Goa"
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
