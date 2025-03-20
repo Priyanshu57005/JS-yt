45b4f6ceee14efdcf9b1a9af773d2af0032648cf
Priyanshu Gautam <priyanshu57005@gmail.com>
2025-03-20 16:23:41 +0000

    conversion done

---
+++ 01_Basic/03_ConversionorOperation.js
@@ -0,0 +1,27 @@
+//let score = "33" //-> string
+//let score = "33abc" //-> NaN
+//let score = true //-> boolean
+//let score = null //-> object
+//let score = undefined //-> undefined
+
+//let score = "Priyanshu" //-> string
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//let isLoggedIn = 1 //-> true
+//let isLoggedIn = "" // -> false //0
+let isLoggedIn = "Priyanshu" // -> false
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+
+let sringNumber = String(someNumber)
+console.log (sringNumber)
+console.log( typeof sringNumber) //-> string
